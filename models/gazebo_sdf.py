5fba8bdb33e12c3e8e3f45991f355b3900727f76
Matheus Laranjeira <matheuslaranjeira1@gmail.com>
2016-12-01 04:08:46 +0100

    python script to generate the model turtle1 + rope + turtle2 working. Tested with a 30cm rope orange

--- models/gazebo_sdf.py
+++ models/gazebo_sdf.py
@@ -11,6 +11,14 @@ def create_cylinder(root,radius,length):
     RADIUS.text = str(radius)
     LENGTH.text = str(length)
     return root
+    
+    
+def create_sphere(root,radius):
+    GEOMETRY = ltr.SubElement(root,"geometry")
+    SPHERE = ltr.SubElement(GEOMETRY,"sphere")
+    RADIUS = ltr.SubElement(SPHERE,"radius")
+    RADIUS.text = str(radius)
+    return root
 
 def create_inertial(root,rope_link):
     POSE = ltr.SubElement(root, "pose")
@@ -39,12 +47,49 @@ def create_surface(root,rope_link):
     ODE = ltr.SubElement(CONTACT,"ode")
     MIN_DEPTH = ltr.SubElement(ODE,"min_depth")
     MIN_DEPTH.text = str(rope_link.min_depth)
-    FRICTION = ltr.SubElement(SURFACE,"contact")
-    ODEf = ltr.SubElement(CONTACT,"ode")
+    FRICTION = ltr.SubElement(SURFACE,"friction")
+    ODEf = ltr.SubElement(FRICTION,"ode")
     MU = ltr.SubElement(ODEf,"mu")
     MU.text = str(rope_link.mu)
     MU2 = ltr.SubElement(ODEf,"mu2")
     MU2.text = str(rope_link.mu2)
+    return root
+
+def create_material(root,rope_link):
+    AMBIENT	= ltr.SubElement(root,"ambient")
+    AMBIENT.text = str(rope_link.color.red)+" "+ str(rope_link.color.green)+" "+ str(rope_link.color.blue) + " " + str(rope_link.color.alpha)
+    DIFFUSE	= ltr.SubElement(root,"diffuse")
+    DIFFUSE.text = str(rope_link.color.red)+" "+ str(rope_link.color.green)+" "+ str(rope_link.color.blue) + " " + str(rope_link.color.alpha)
+    SPECULAR = ltr.SubElement(root,"specular")
+    SPECULAR.text = str(0.1)+" "+ str(0.1)+" "+ str(0.1) + " " + str(1)
+    EMISSIVE	= ltr.SubElement(root,"emissive")
+    EMISSIVE.text = str(0)+" "+ str(0)+" "+ str(0) + " " + str(0)
+    return root
+    
+def create_collision(root,rope_link):
+    POSE = ltr.SubElement(root, "pose")
+    pose = rope_link.inertial.pose
+    POSE.text 	= str(pose.x) +" "+str(pose.y)+" "+str(pose.z)+" "+ str(pose.phi) +" "+ str(pose.theta) +" "+str(pose.psi)
+    create_cylinder(root,rope_link.radius,rope_link.length)
+    create_surface(root, rope_link)
+    return root
+
+def create_visual(root,rope_link):
+    POSE = ltr.SubElement(root, "pose")
+    pose = rope_link.inertial.pose
+    POSE.text 	= str(pose.x) +" "+str(pose.y)+" "+str(pose.z)+" "+ str(pose.phi) +" "+ str(pose.theta) +" "+str(pose.psi)
+    create_cylinder(root,rope_link.radius,rope_link.length)
+    MATERIAL = ltr.SubElement(root, "material")
+    create_material(MATERIAL,rope_link)
+    return root
+    
+def create_visual_sphere(root,rope_link):
+    POSE = ltr.SubElement(root, "pose")
+    pose = rope_link.sphere.pose
+    POSE.text 	= str(pose.x) +" "+str(pose.y)+" "+str(pose.z)+" "+ str(pose.phi) +" "+ str(pose.theta) +" "+str(pose.psi)
+    create_sphere(root,rope_link.radius)
+    MATERIAL = ltr.SubElement(root, "material")
+    create_material(MATERIAL,rope_link)
     return root
 
 def create_rope_link(root,rope_link):
@@ -54,23 +99,60 @@ def create_rope_link(root,rope_link):
     INERTIAL	= ltr.SubElement(LINK, "inertial")
     create_inertial(INERTIAL,rope_link)
     COLLISION 	= ltr.SubElement(LINK, "collision", name=rope_link.name + "_collision")
-    create_cylinder(COLLISION,rope_link.radius,rope_link.length)
-    create_surface(COLLISION, rope_link)
+    create_collision(COLLISION,rope_link)
     VISUAL 		= ltr.SubElement(LINK, "visual",name=rope_link.name+"_visual")
-    create_cylinder(VISUAL,rope_link.radius,rope_link.length)
+    create_visual(VISUAL,rope_link)
+    VISUAL_SPH	= ltr.SubElement(LINK, "visual",name=rope_link.name+"_visual_sphere")
+    create_visual_sphere(VISUAL_SPH,rope_link)
     return root
 
-def create_rev_joint(root,joint_name,parent,child,pose,xyz):
-    JOINT = ltr.SubElement(root, "joint", type="revolute", name=joint_name)
-    POSE = ltr.SubElement(JOINT, "pose")
-    POSE.text = str(pose)
-    PARENT = ltr.SubElement(JOINT,"parent")
-    PARENT.text = str(parent)
-    CHILD = ltr.SubElement(JOINT,"child")
-    CHILD.text = str(child)
-    AXIS = ltr.SubElement(JOINT,"axis")
+def create_axis(root,rope_joint):
+    AXIS = ltr.SubElement(root,"axis")
     XYZ = ltr.SubElement(AXIS,"xyz")
-    XYZ.text = str(xyz)
+    XYZ.text = "0 1 0"
+    LIMIT = ltr.SubElement(AXIS,"limit")
+    LOWER = ltr.SubElement(LIMIT,"lower")
+    LOWER.text = str(rope_joint.lowlim)
+    UPPER = ltr.SubElement(LIMIT,"upper")
+    UPPER.text = str(rope_joint.upplim)
+    DYNAM = ltr.SubElement(AXIS,"dynamics")
+    DAMPI = ltr.SubElement(DYNAM,"damping")
+    DAMPI.text = str(rope_joint.damping)
+    
+def create_axis2(root,rope_joint):
+    AXIS2 = ltr.SubElement(root,"axis2")
+    XYZ2 = ltr.SubElement(AXIS2,"xyz")
+    XYZ2.text = "0 0 1"
+    LIMIT = ltr.SubElement(AXIS2,"limit")
+    LOWER = ltr.SubElement(LIMIT,"lower")
+    LOWER.text = str(rope_joint.lowlim)
+    UPPER = ltr.SubElement(LIMIT,"upper")
+    UPPER.text = str(rope_joint.upplim)
+    DYNAM = ltr.SubElement(AXIS2,"dynamics")
+    DAMPI = ltr.SubElement(DYNAM,"damping")
+    DAMPI.text = str(rope_joint.damping)
+    
+def create_joint_physics(root):
+    PHYSICS = ltr.SubElement(root,"physics")
+    ODE = ltr.SubElement(PHYSICS,"ode")
+    CFM = ltr.SubElement(ODE,"cfm_damping")
+    CFM.text = str(1)
+    return root
+
+
+def create_univ_joint(root,rope_joint):
+    JOINT = ltr.SubElement(root, "joint", type="universal", name=rope_joint.name)
+    POSE = ltr.SubElement(JOINT, "pose")
+    pose = rope_joint.pose
+    POSE.text 	= str(pose.x) +" "+str(pose.y)+" "+str(pose.z)+" "+ str(pose.phi) +" "+ str(pose.theta) +" "+str(pose.psi)
+    PARENT = ltr.SubElement(JOINT,"parent")
+    PARENT.text = str(rope_joint.parent)
+    CHILD = ltr.SubElement(JOINT,"child")
+    CHILD.text = str(rope_joint.child)
+    create_axis(JOINT,rope_joint)
+    create_axis2(JOINT,rope_joint)
+    create_joint_physics(JOINT)
+
     return root
     
     
--- models/model_creator.py
+++ models/model_creator.py
@@ -9,11 +9,13 @@ The model configuration can be changed editing the file settings.config
 import lxml.etree as ltr
 import decimal
 from gazebo_sdf import *
+from math import log10
 
 class Color:
 	red = 0.0
 	green = 0.0
 	blue = 0.0
+	alpha = 1.0
 
 	def __init__(self, red, green, blue):
 		self.red = red
@@ -55,6 +57,15 @@ class Inertial:
 		self.iyy = iyy
 		self.iyz = iyz
 		self.izz = izz
+		
+class Sphere:
+	pose = Pose
+	radius = 0.0
+
+
+	def __init__(self, pose, radius):
+		self.pose = pose
+		self.radius = radius
 
 class Rope_link:
 	name = ""
@@ -66,6 +77,7 @@ class Rope_link:
 	mu2 = 0.0
 	inertial = Inertial
 	color = Color
+	sphere = Sphere
 
 	def __init__(self, name, pose, radius, length, min_depth, mu, mu2, inertial, color):
 		self.name = name
@@ -77,6 +89,23 @@ class Rope_link:
 		self.mu2 = mu2
 		self.inertial = inertial
 		self.color = color
+		
+class Rope_joint:
+	name = ""
+	parent = "parent"
+	child = "child"
+	pose = Pose
+	lowlim = -1.57
+	upplim = 1.57
+	damping = 0.1
+	cfm_damping = 1
+
+	def __init__(self, name, parent, child, pose, lowlim, upplim, damping):
+		self.name = name
+		self.pose = pose
+		self.lowlim = lowlim
+		self.upplim = upplim
+		self.damping = damping
 
 file = open('settings.config','r')  
 for line in file:
@@ -89,14 +118,59 @@ file.close
 ROOT = ltr.Element("sdf", version="1.4")
 MODEL = ltr.SubElement(ROOT, "model", name = "tethered_turtlebots")
 
-# Create rope_link
+# Create link object
+linkname = "link_0"
 frame_i = Pose(0.1350, 0.0000, 0.4100, 0.0000, 0.0000, 0.0000)
 frame_o = Pose(0.0000, 0.0000, 0.0000, 0.0000, 1.5708, 0.0000)
+frame_s = Pose(-0.5*length, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000)
 inertial = Inertial(frame_o,mass,ixx,ixy,ixz,iyy,iyz,izz)
 color = Color(1, 0.25, 0)
-rope_link = Rope_link("link_00", frame_i, radius, length, min_depth, mu, mu2, inertial, color)
-create_rope_link(MODEL,rope_link)
+rope_link = Rope_link(linkname, frame_i, radius, length, min_depth, mu, mu2, inertial, color)
+rope_link.sphere.pose = frame_s
+# Create joint object
+jointname = "joint_0"
+jointpose = Pose(-0.5*length, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000)
+lowlim = -1.57
+upplim = 1.57
+parent  = "parent"
+child = "child"
+rope_joint = Rope_joint(jointname, parent, child, jointpose,lowlim,upplim,damping)
+for i in range(nelem):
+	# Define link name and joint name, parent and child link
+	if(i == 0):
+		# link and joint index = i
+		rope_link.name = rope_link.name.replace(rope_link.name[-1],str(i))
+		rope_joint.name = rope_joint.name.replace(rope_joint.name[-1],str(i))
+		# if first joint, parent link is robot1 attachment point (base_foot1print)
+		rope_joint.parent = "base_foot1print"
+	elif(i > 0 and log10(i) <= 1.0):
+		# joint parent link is rope previous link
+		rope_joint.parent = rope_link.name
+		# update rope link with current index
+		rope_link.name = rope_link.name.replace(rope_link.name[-1],str(i))
+		# update joint with current index
+		rope_joint.name = rope_joint.name.replace(rope_joint.name[-1],str(i))
+	elif(log10(i) > 1.0 and  log10(i) < 2.0):
+		rope_joint.parent = rope_link.name
+		rope_link.name = rope_link.name.replace(rope_link.name[-2:],str(i))
+		rope_joint.name = rope_joint.name.replace(rope_joint.name[-2:],str(i))
+	# joint child is always the current link
+	rope_joint.child = rope_link.name
+	# Add link to model
+	create_rope_link(MODEL,rope_link)
+	# Update pose to add following link in next loop
+	rope_link.pose.x = rope_link.pose.x + rope_link.length
+	# Add joint to model
+	create_univ_joint(MODEL,rope_joint)
+# Add final joint attaching the rope to the leader
+rope_joint.name = "joint_"+str(nelem)
+rope_joint.pose = Pose(0.5*length, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000)
+rope_joint.parent = "base_foot2print"
+rope_joint.child = rope_link.name
+create_univ_joint(MODEL,rope_joint)
 
+
+	
 # WRITE THE MODEL
 f = open(output, 'w')
 f.write('<?xml version="1.0" ?>\n')
